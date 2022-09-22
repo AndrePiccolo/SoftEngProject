0aa85d12fdb9fefcc8fa4fbfaabff3f28ed39754
AndrePiccolo <andrepiccolo@gmail.com>
2022-09-22 12:31:59 -0700

    First Version

---
+++ Code/rentDog/src/main/java/com/douglas/ConfigProperties.java
@@ -0,0 +1,10 @@
+package com.douglas;
+
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.stereotype.Component;
+
+//@ConfigurationProperties
+@Configuration
+public class ConfigProperties {
+}
---
+++ Code/rentDog/src/test/java/com/douglas/rentDog/RentDogApplicationTests.java
@@ -0,0 +1,13 @@
+package com.douglas.rentDog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RentDogApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
